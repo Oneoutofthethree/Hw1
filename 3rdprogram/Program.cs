d08bac296f5100e0c1274845dcf44ba7b313bee5
One Outofthethree <hku1up@outlook.com>
2023-03-27 00:47:56 +0300

    added 3rd program

---
+++ 3rdprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+
+int a = int.Parse(Console.ReadLine());
+
+if(a%2==0)
+{
+    Console.WriteLine("Число является четным.");
+}
+if (a%2>0)
+{
+    Console.WriteLine("Введено нечетное число.");
+}
