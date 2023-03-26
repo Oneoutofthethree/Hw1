d176d4850cde155b7ec2784ee381b7f59d018d9a
One Outofthethree <hku1up@outlook.com>
2023-03-27 00:46:06 +0300

    added 2nd program

---
+++ 2ndprogram/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите числа:");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(b>a) max =b;
+if(c>b) max =c; 
+string tmp = $"Наибольшее число:{max}";
+
+Console.WriteLine(tmp);
