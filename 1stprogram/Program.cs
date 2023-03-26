d58eb92889eb0dcf791e29d6d22c47f2992390e2
One Outofthethree <hku1up@outlook.com>
2023-03-27 00:44:09 +0300

    added 1st program

---
+++ 1stprogram/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите числа:");
+int N = int.Parse(Console.ReadLine());
+
+int F = int.Parse(Console.ReadLine());
+
+if (N > F)
+{
+    Console.WriteLine("Максимум равен:");
+    Console.WriteLine(N);
+    Console.WriteLine("Минимум равен:");
+    Console.WriteLine(F);
+}
+if (N < F)
+{
+    Console.WriteLine("Максимум равен:");
+    Console.WriteLine(F);
+    Console.WriteLine("Минимум равен:");
+    Console.WriteLine(N);  
+}
